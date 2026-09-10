c0a0fd09c31e8ee48e557cefc65ebac8bf99b539
Guilherme Gontijo <98989904+GlGontijo@users.noreply.github.com>
2026-09-10 14:50:28 -0300

    Fix repository messages and suite variables in post-install.sh
    Updated repository messages and suite variable for ceph and pve-test repositories.

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -92,26 +92,27 @@ Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 EOF
 msg_ok "Repositório no-subscription habilitado"
 
-msg_info "Adicionando 'repositório ceph' (deb822)"
+msg_info "Adicionando repositório 'ceph no-subscription' (deb822)"
 cat >"$CEPH_SOURCES" <<EOF
 Types: deb
 URIs: http://download.proxmox.com/debian/${CEPH_RELEASE}
-Suites: $CODNAME
+Suites: $CODENAME
 Components: no-subscription
 Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+Enabled: false
 EOF
-msg_ok "Added 'ceph package repositories' (${CEPH_RELEASE})"
+msg_ok "Repositório 'ceph no-subscription' (${CEPH_RELEASE}) adicionado, mas desabilitado. Caso seja necessário o uso, habilitar via web"
 
-msg_info "Adding 'pve-test' repository (deb822, disabled)"
+msg_info "Adicionando repositório 'pve-test' (deb822, disabled)"
 cat >"$TEST_SOURCES" <<EOF
 Types: deb
 URIs: http://download.proxmox.com/debian/pve
-Suites: trixie
+Suites: $CODENAME
 Components: pve-test
 Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 Enabled: false
 EOF
-msg_ok "Added 'pve-test' repository"
+msg_ok "Repositório 'pve-test' adicionado, mas desabilitado. Caso seja necessário o uso, habilitar via web"
 
 # ── 3) Remove nag de assinatura na UI ───────────────────────────
 msg_info "Removendo aviso de assinatura na Web UI"
