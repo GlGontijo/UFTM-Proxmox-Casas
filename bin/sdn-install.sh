476f3ef0cbaec9b4c491caf11105deb593e245ee
Guilherme Gontijo <98989904+GlGontijo@users.noreply.github.com>
2026-09-11 16:34:16 -0300

    Update NODE_ID to use UFTM_HOSTNAME_FINAL

--- bin/sdn-install.sh
+++ bin/sdn-install.sh
@@ -98,12 +98,12 @@ else
   msg_warn "pvesh falhou ao registrar o nó hub -- verifique /tmp/uftm-pvesh-err.log (schema ainda não confirmado, ver comentário acima)"
 fi
 
-NODE_ID="${FABRIC_ID}_${UFTM_HOSTNAME}"
+NODE_ID="${FABRIC_ID}_${UFTM_HOSTNAME_FINAL}"
 msg_info "Registrando nó SPOKE ($UFTM_HOSTNAME), peer = hub ($HUB_HOSTNAME)"
 IFACE_STR="name=${WG_IFACE},listen_port=${UFTM_WG_PORT},public_key=${WG_PUBKEY},ip=${UFTM_WG_TUNNEL_IP}/24"
 PEER_STR="type=external,node=${HUB_HOSTNAME},iface=${WG_IFACE}"
 if pvesh_try create "$NODE_COLLECTION" \
-    -node_id "$UFTM_HOSTNAME" -allowed-ips "$FABRIC_ALLOWED_IPS" \
+    -node_id "$UFTM_HOSTNAME_FINAL" -allowed-ips "$FABRIC_ALLOWED_IPS" \
     -endpoint "${UFTM_IP_WAN:-auto}" -role internal \
     -interfaces "$IFACE_STR" -peers "$PEER_STR" ; then
   msg_ok "Nó spoke registrado"
