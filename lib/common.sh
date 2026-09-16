f8b879c00892e7031cc42149ad5b1981717d7f3d
Guilherme Gontijo <98989904+GlGontijo@users.noreply.github.com>
2026-09-16 15:40:07 -0300

    Refactor directory path variables in common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -11,7 +11,8 @@ msg_ok()    { echo -e "${BFR} ${CM} ${GN}$1${CL}\n"; }
 msg_error() { echo -e "${BFR} ${CROSS} ${RD}$1${CL}\n"; }
 msg_warn()  { echo -e " ${YW}⚠ $1${CL}\n"; }
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)/.."
+LIB_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+SCRIPT_DIR="$(cd -- "${LIB_DIR}/.." &>/dev/null && pwd)"
 UFTM_ETC_DIR="/etc/uftm-proxmox-casas"
 UFTM_BACKUP_ROOT="${SCRIPT_DIR}/root/uftm-proxmox-casas-backups"
 UFTM_CACHE_DIR="${SCRIPT_DIR}/root/uftm-proxmox-casas-cache"
